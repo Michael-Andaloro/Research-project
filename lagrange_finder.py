dd2eae13e532654d436705880fa79425e091358e
Michael <andaloromichael@gmail.com>
2020-11-12 16:12:29 -0600

    Working on finding lagrange points

--- lagrange_finder.py
+++ lagrange_finder.py
@@ -15,18 +15,26 @@ vector of L4
 vector of L5
 """
 
+# Notes:
+# temp = vec(10, 20, 30)
+# temp.x = 10.0, etc
+
 def lagrange_finder(star_1, star_2):
     # Find lagrange point 1
-    print("Hi")
+    
     # Find lagrange point 2
     
-    # Find lagrange point 3
-    
-    # Find lagrange point 4
-    
-    # Find lagrange point 5
+    # Find lagrange point 3 (opposite the point of star_2)
+    lp2 = -star_2
 
-    
+    # Find lagrange point 4 (equilateral triangle)
+    # Find halfway point between the two
+    halfway = star_2.x - star_1.x
+    print(halfway)
+
+    # Find lagrange point 5 (equilateral triangle)
+
+    return lp2
 if __name__ == '__main__':
     print("Running test case") 
-    lagrange_finder(vec(10, 20, 30), vec(10, 40, 70))
+    lagrange_finder(vec(10, 20, 30), vec(20, 40, 70))
